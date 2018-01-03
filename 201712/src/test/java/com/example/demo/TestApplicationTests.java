7306749e1fab8a2e971a33fdfef93176e2f13416
Chunmei <chunmei.wu@innovergroup.com>
2018-01-03 16:18:42 +0800

    Unit test

--- 201712/src/test/java/com/example/demo/TestApplicationTests.java
+++ 201712/src/test/java/com/example/demo/TestApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +13,15 @@ public class TestApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void userTest(){
+		restTemplate.getForEntity("/api/user/1", String.class);
+		//或者使用mockMvc
 	}
 
 }
