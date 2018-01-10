35644a6293fc1ac6a7897494541ba50ca39d4e3f
Chunmei <chunmei.wu@innovergroup.com>
2018-01-10 14:22:37 +0800

    消费者， 生产者

---
+++ 201712/src/main/java/com/example/demo/produceconsumer/Producer.java
@@ -0,0 +1,24 @@
+package com.example.demo.produceconsumer;
+
+/**
+ * Created by chunmei on 12/28/2017.
+ */
+public class Producer extends Thread{
+    private StorageWithReentrantLock storage;
+    int num;
+    public Producer(StorageWithReentrantLock storage, int num)
+    {
+        this.storage = storage;
+        this.num = num;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            storage.Produce(num);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
